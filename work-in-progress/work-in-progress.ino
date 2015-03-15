6b8c1612ed125025cf396ad7d02244869dba9675
Jonas Wippermann <jonas@vfuc.co>
2015-03-15 21:21:11 +0100

    added choreo function for Chimes by Hudson Mohawke

--- work-in-progress/work-in-progress.ino
+++ work-in-progress/work-in-progress.ino
@@ -12,6 +12,44 @@ void setup() {
   FastLED.addLeds<NEOPIXEL, PIN>(leds, NUM_LEDS);
 }
 
+
 void loop() {
-  jiggly.runAllAnimations(leds, NUM_LEDS, 40, 2000);
+//  jiggly.runAllAnimations(leds, NUM_LEDS, 40, 2000);
+  chimes();
+}
+
+
+void chimes(){
+  jiggly.runner(leds, NUM_LEDS, CRGB(70,0,100), 6600);
+  jiggly.runner(leds, NUM_LEDS, CRGB(100,60,10), 6600);
+//  jiggly.runner(leds, NUM_LEDS, CRGB(0,100,30), 6600);
+  
+  jiggly.flashyShit(leds, NUM_LEDS, 50, 6600);
+  jiggly.flashyShit(leds, NUM_LEDS, 50, 6600);
+  
+  jiggly.progressbarCentered(leds, NUM_LEDS, CRGB(30,0,100), 3300);
+  delay(390);
+  for (int i = 0; i < 3; i++){
+    jiggly.blinking(leds,NUM_LEDS,CRGB(0,60,100), 200);
+  }
+  jiggly.blinking(leds,NUM_LEDS,CRGB(0,60,100), 300);
+  jiggly.blinking(leds,NUM_LEDS,CRGB(0,60,100), 210);
+  
+  
+  jiggly.progressbarCentered(leds, NUM_LEDS, CRGB(100,0,0), 3300);
+  delay(395);
+  for (int i = 0; i < 3; i++){
+    jiggly.blinking(leds,NUM_LEDS,CRGB(100,20,20), 200);
+  }
+  jiggly.blinking(leds,NUM_LEDS,CRGB(100,20,20), 300);
+  jiggly.blinking(leds,NUM_LEDS,CRGB(100,20,20), 210);
+  
+  jiggly.progressbarCentered(leds, NUM_LEDS, CRGB(0,100,30), 3300);
+  delay(395);
+  for (int i = 0; i < 3; i++){
+    jiggly.blinking(leds,NUM_LEDS,CRGB(0,100,40), 200);
+  }
+  jiggly.blinking(leds,NUM_LEDS,CRGB(0,100,40), 300);
+  jiggly.blinking(leds,NUM_LEDS,CRGB(0,100,40), 210);
+  delay(10000);
 }

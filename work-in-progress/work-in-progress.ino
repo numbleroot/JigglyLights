e1a1bb0c56fe0014164433243d6997257ece19a8
Lennart Oldenburg <hello@lennartoldenburg.de>
2015-03-20 02:05:52 +0100

    Started input dependant effect controls.

--- work-in-progress/work-in-progress.ino
+++ work-in-progress/work-in-progress.ino
@@ -23,10 +23,46 @@ JigglyLights jiggly;
 
 
 void setup() {
+  Serial.begin(57600);
   FastLED.addLeds<NEOPIXEL, PIN>(leds, NUM_LEDS);
 }
 
 
 void loop() {
-  jiggly.matrixLevel(leds, LEDS_X, LEDS_Y, CRGB(0, 0, 5), 200);
+  int doIt;
+  
+  if(Serial.available() > 0) {
+  
+    doIt = Serial.read();
+    Serial.println(doIt, DEC);
+    
+    if(doIt == 49) {
+      jiggly.matrixWave(leds, LEDS_X, LEDS_Y, CRGB(0, 0, 2), 500);
+      jiggly.clearAll(leds, NUM_LEDS);
+    }
+    else if(doIt == 50) {
+      jiggly.matrixWaveInverted(leds, LEDS_X, LEDS_Y, CRGB(0, 0, 2), 500);
+      jiggly.clearAll(leds, NUM_LEDS);
+    }
+    else if(doIt == 51) {
+      jiggly.matrixWaveOne(leds, LEDS_X, LEDS_Y, CRGB(0, 0, 2), 500);
+      jiggly.clearAll(leds, NUM_LEDS);
+    }
+    else if(doIt == 52) {
+      jiggly.matrixWaveOneRandom(leds, LEDS_X, LEDS_Y, CRGB(0, 0, 2), 500);
+      jiggly.clearAll(leds, NUM_LEDS);
+    }
+    else if(doIt == 53) {
+      jiggly.matrixProgressbar(leds, LEDS_X, LEDS_Y, CRGB(0, 0, 2), 1000);
+      jiggly.clearAll(leds, NUM_LEDS);
+    }
+    else if(doIt == 54) {
+      jiggly.matrixProgressbarCentered(leds, LEDS_X, LEDS_Y, CRGB(0, 0, 2), 1000);
+      jiggly.clearAll(leds, NUM_LEDS);
+    }
+    else if(doIt == 55) {
+      jiggly.matrixLevel(leds, LEDS_X, LEDS_Y, CRGB(0, 0, 2), 500);
+      jiggly.clearAll(leds, NUM_LEDS);
+    }
+  }
 }
